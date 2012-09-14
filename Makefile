7c0fbd4853a1d3571ac08d4dd161b153fe02ac78
David Champion <dgc@uchicago.edu>
2012-09-14 00:13:02 -0500

    Extend algorithm, use getpwnam_r().
    The nss_identity group functions now follow this logic:
    
    A. If request is for a group by GID:
       1. If GID corresponds to a valid UID, obtain the passwd entry
          for this UID and construct a grp entry based upon that UID(GID) and
          pw_name.  Put the identified user into the group.
       2. Otherwise, construct a grp entry using the requested GID and and
          the group name "group_####", where "####" is the decimal ASCII
          encoding of the GID.
    
    B. If request is for a group by name:
       1. If group name corresponds to a valid user name, obtain the passwd entry
          for this user and construct a grp entry based upon that UID(GID) and
          the requested group name.
    
       2. Otherwise, if group name matches "group_####", extract the #### as
          a GID and perform GID lookup (see ruleset A) using that GID.
    
       3. Otherwise, fail to discover the group.
    
    Note that the group entries thus fabricated are not necessarily
    reversible!  For example, if user "abc" has uid 1234, then a lookup on
    gid=1234 will yield the group entry "abc::1234:abc".  A lookup on group
    name="abc" will yield the same -- this is reversible.  However, a lookup
    on "group_1234" will yield the group entry "group_1234:xx:1234:abc".
    That cannot be reversed back to group_1234 because that name is
    fabricated in the presence of a user "abc" with uid 1234.

--- Makefile
+++ Makefile
@@ -4,4 +4,4 @@ libnss_identity.so: nss_identity.o
 	ld -G -o libnss_identity.so nss_identity.o
 
 nss_identity.o: nss_identity.c
-	gcc -c -shared nss_identity.c 
+	gcc -c -shared nss_identity.c
--- nss_identity.c
+++ nss_identity.c
@@ -4,7 +4,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <unistd.h>
-#include <stdlib.h>
+#include <stdlib.h> /* sysconf() */
 
 enum nss_status
 group(const char *name, gid_t gid, struct group *grp,
@@ -12,9 +12,11 @@ group(const char *name, gid_t gid, struct group *grp,
 {
 	struct passwd *pw, *pwp;
 	char *bufp;
-	int len;
+	int len = 0;
 	int maxpwbuflen;
 	char *pwdata = NULL;
+	char grname[1024];
+	int r;
 
 #ifdef DEBUG
 	printf("name=%s, gid=%d, buf=%p, buflen=%d\n", name, (int) gid, buf, buflen);
@@ -27,41 +29,92 @@ group(const char *name, gid_t gid, struct group *grp,
 	/* We'll compute the required buffer length. If enough space is not
 	 * given to us, we can return an error. */
 
-	/* Init required length as size of group name */
-	len = strlen(name) + 1;
+	/* Prepare for getpw*_r calls */
+	maxpwbuflen = sysconf(_SC_GETPW_R_SIZE_MAX);
+	pwdata = malloc(maxpwbuflen);
+	pw = malloc(sizeof(struct passwd));
 
-	/* Add empty password string to requirements */
-	len += 1;
+	if (name) {
+		/* We are performing getgrnam */
+		r = getpwnam_r(name, pw, pwdata, maxpwbuflen, &pwp);
 
-	/* Search for a user with uid == gid */
-	pw = getpwuid((uid_t) gid);
-	if (pw == NULL) {
+		/* If no match, but group matches group_####, extract gid */
+		if (r != 0 || pwp == NULL) {
+			if (!strncmp(name, "group_", 6)) {
+				char *p, *end;
+
+				/* Extract gid from requested name */
+				p = (char *) &name[6];
+				gid = (gid_t) strtoul(p, &end, 10);
+
+				/* Convert to gid request */
+				r = getpwuid_r((uid_t) gid, pw, pwdata, maxpwbuflen, &pwp);
+			}
+		}
+	}
+	else {
+		/* We are performing getgrgid - search for a user with uid == gid */
+		r = getpwuid_r((uid_t) gid, pw, pwdata, maxpwbuflen, &pwp);
+	}
+
+#ifdef DEBUG
+	printf("r = %d, pwp = %p, name = %p, gid = %d\n", r, pwp, name, gid);
+#endif
+
+	if (r != 0 || pwp == NULL) {
 #ifdef DEBUG
 		printf("no\n");
 #endif
 		/* Not found, add space for empty member list to requirements */
 		len += sizeof(char *);
 	}
+
 	else {
 #ifdef DEBUG
 		printf("yes\n");
 #endif
 		/* If found, add space for 1-element member list to requirements */
 		len += sizeof(char *) * 2;
-		len += strlen(pw->pw_name) + 1;
+		len += strlen(pwp->pw_name) + 1;
 	}
+
+	if (pwp && name == NULL) {
+		name = pwp->pw_name;
+	}
+	else if (name == NULL) {
+		snprintf(grname, sizeof(grname), "group_%d", gid);
+		name = grname;
+	}
+	else if (pwp && gid == -1) {
+		gid = (gid_t) pwp->pw_uid;
+	}
+	else if (gid == -1) {
+		/* Free transient data */
+		if (pwp && pwp != pw)
+			free(pwp);
+		free(pw);
+		free(pwdata);
+		return NSS_STATUS_NOTFOUND;
+	}
+
+	/* Init required length with size of group name */
+	len += strlen(name) + 1;
+
+	/* Add empty password string to requirements */
+	len += 1;
 
 	/* If not enough space provided, return */
 	if (buflen < len) {
 		/* Free transient data */
+		if (pwp && pwp != pw)
+			free(pwp);
+		free(pw);
+		free(pwdata);
 		return NSS_STATUS_TRYAGAIN;
 	}
 
 	/* Point to provided buffer.  We will stack on internal strings. */
 	bufp = buf;
-
-	if (pw)
-		name = pw->pw_name;
 
 	/* Stack on group name */
 	strcpy(bufp, name);
@@ -77,11 +130,11 @@ group(const char *name, gid_t gid, struct group *grp,
 	grp->gr_gid = gid;
 
 	/* Stack on group member list */
-	if (pw) {
+	if (pwp) {
 		char *pwname, **gp;
 
-		strcpy(pwname = bufp, pw->pw_name);
-		bufp += strlen(pw->pw_name) + 1;
+		strcpy(pwname = bufp, pwp->pw_name);
+		bufp += strlen(pwp->pw_name) + 1;
 
 		grp->gr_mem = gp = (char **)bufp;
 		*gp++ = pwname;
@@ -93,6 +146,12 @@ group(const char *name, gid_t gid, struct group *grp,
 		*gp++ = NULL;
 	}
 
+	/* Free transient data */
+	if (pwp && pwp != pw)
+		free(pwp);
+	free(pw);
+	free(pwdata);
+
 	*result = grp;
 	return NSS_STATUS_SUCCESS;
 }
@@ -102,25 +161,13 @@ enum nss_status
 _nss_identity_getgrnam_r(const char *name, struct group *grp,
                          char *buf, size_t buflen, struct group **result)
 {
-	gid_t gid;
-	char *p, *end;
-
 #ifdef DEBUG
 	printf("nss_identity_getgrgid_r\n");
 	fflush(stdout);
 #endif
 
-	if (strncmp(name, "group", 5))
-		return NSS_STATUS_NOTFOUND;
-
-	/* Extract gid from requested name */
-	p = (char *) &name[5];
-	if (*p == '_')
-		p++;
-	gid = (gid_t) strtoul(p, &end, 10);
-
 	/* Hand off to generic group backend */
-	return group(name, gid, grp, buf, buflen, result);
+	return group(name, -1, grp, buf, buflen, result);
 }
 
 
@@ -128,17 +175,13 @@ enum nss_status
 _nss_identity_getgrgid_r(gid_t gid, struct group *grp,
                          char *buf, size_t buflen, struct group **result)
 {
-	char name[1024];
-
 #ifdef DEBUG
 	printf("nss_identity_getgrgid_r\n");
 	fflush(stdout);
 #endif
 
-	snprintf(name, sizeof(name), "group_%d", (int) gid);
-
 	/* Hand off to generic group backend */
-	return group(name, gid, grp, buf, buflen, result);
+	return group(NULL, gid, grp, buf, buflen, result);
 }
 
 
